1bb915b7863f5725d1165515b751735d8ef88da6
namanverma991 <nnamanvverma@gmail.com>
2024-11-05 11:09:18 +0530

    if-else

---
+++ flow control/if-else.py
@@ -0,0 +1,79 @@
+#check number is odd or even
+# n = int(input("enter a number:"))
+# if n%2 == 0:
+#     print(n,"is even")
+# else:
+#     print(n,"is odd")
+
+
+#check number is positive, negative or zero
+# n = int(input("enter a number"))
+# if n > 0:
+#     print(n,"is positive")
+# elif n == 0:
+#     print(n,"is zero")
+# else:
+#     print(n,"is negative")
+
+
+#check number 
+# n = int(input("enter a number:"))
+# if n%2 == 0 and n > 0:
+#     print(n,"is positive even")
+# elif n%2 == 1:
+#     print(n,"is positive odd")
+# elif n%2 == 0 and n < 0:
+#     print(n,"is negative even")
+# elif n%2 == 1 and n < 0:
+#     print(n,"is negative odd")
+# else:
+#     print(n,"is zero")
+
+
+#check the largest number 
+# n = int(input("enter a number:"))
+# m = int(input("enter a number:"))
+# o = int(input("enter a number:"))
+# if n > m and n > o:
+#     print(n,"is the greatest")
+# elif m > o:
+#     print(m,"is the greatest")
+# else:
+#     print(o,"is the greatest")
+
+
+#leap year
+# y = int(input("enter a yer:"))
+# if y%4 == 0 and y%100 != 0:
+#     print(y,'is leap year')
+# if y%400 == 0:
+#     print(y,"is leap year")
+# else:
+#     print(y,"not leap year")
+
+
+#calculator program
+# import sys
+# print("enter a opearator:\n 1: add\n 2: subtract\n 3: multiply")
+# choice = int(input("enter a choice:"))
+# if choice not in (1,2,3):
+#     print("invalid choice")
+#     sys.exit()
+
+# a = int(input("enter a number:"))
+# b = int(input("enter a number:"))
+
+# if choice == 1:
+#     res = a+b
+# elif choice == 2:
+#     res = a-b
+# else:
+#     res = a*b
+
+# print("result", res)
+
+a,b,c = 4
+if a+b == 8:
+    print("hii")
+else:
+    print("no")
